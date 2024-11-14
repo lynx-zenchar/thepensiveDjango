6b0e5571c0799e99a33430869ef264ebe63f9834
Matthew Eleazar <meleazar@nd.edu>
2024-11-13 22:41:22 -0500

    fixed some wsgi stuff again

--- thepensive_django/wsgi.py
+++ thepensive_django/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thepensive_django.settings')
 
 application = get_wsgi_application()
+
+# Expose the WSGI callable as a module-level variable named `app`
+app = application
