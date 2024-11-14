f44a26e9fba8c617f03b8bebaf79c9bd65196ad7
Matthew Eleazar <meleazar@nd.edu>
2024-11-13 22:38:40 -0500

    fixed some wsgi stuff

--- thepensive_django/settings.py
+++ thepensive_django/settings.py
@@ -4,18 +4,16 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^s%x#cyz4%ft1rm2=_udap(($(iy+g5fbjqcycgfa48#p3#hnq'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['.vercel.app', '*']
-
 
 # Application definition
 
@@ -54,7 +52,6 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                # Default processors
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',  # Required by auth
                 'django.contrib.auth.context_processors.auth',
@@ -65,7 +62,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'thepensive_django.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
@@ -106,7 +102,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
@@ -114,16 +109,13 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',  # Using pathlib
-    # os.path.join(BASE_DIR, 'static'),  # Using os.path.join
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Directory for collectstatic
 
 # Media files (Uploaded content)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'  # Using pathlib
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Using os.path.join
-
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -139,8 +131,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Custom User Model (if using)
 AUTH_USER_MODEL = 'users.CustomUser'
-
-
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
 CKEDITOR_CONFIGS = {
